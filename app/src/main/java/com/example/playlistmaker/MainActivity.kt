afd989971908e4d2e96bd49bc0dd01a324969fc3
Boris <boris.sulyagin@gmail.com>
2024-04-26 22:47:00 +0300

    Спринт 8/25 → Тема 4/6: Работа в коде → Урок 2/2 Домашнее задание - реализовать нажатия на кнопки главного экрана с заглушками

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search)
+
+        searchButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+        }
+
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library)
+
+        val mediaLibraryButtonListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на медиатеку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        mediaLibraryButton.setOnClickListener(mediaLibraryButtonListener)
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        settingsButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на настройки!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
