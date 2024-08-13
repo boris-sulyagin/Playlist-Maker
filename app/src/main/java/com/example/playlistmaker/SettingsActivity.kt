5bf105455f9a3e27cf431d240a10f18236ca3edd
Boris <boris.sulyagin@gmail.com>
2024-08-14 01:31:24 +0300

    Спринт 9/25 → Тема 1/5: Стили и темы → Урок 3/3

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,10 @@
 package com.example.playlistmaker
-
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
@@ -12,7 +12,7 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
 
         toolbar.setOnClickListener{
             finish()
