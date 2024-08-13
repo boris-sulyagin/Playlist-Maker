6c8e38db03cf709e6185a4ede15e677607e5ea64
Boris <boris.sulyagin@gmail.com>
2024-08-14 02:01:33 +0300

    Спринт 9/25 → Тема 4/5: Хранение данных. Начало → Урок 4/4

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,5 +1,4 @@
 package com.example.playlistmaker
-
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -10,10 +9,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var string: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
 
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.search_close)
@@ -21,13 +23,13 @@ class SearchActivity : AppCompatActivity() {
         clearButton.setOnClickListener{
             inputEditText.setText("")
         }
-
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // empty
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                string = s.toString()
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
@@ -36,9 +38,24 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+        inputEditText.setText(string)
+        if (savedInstanceState != null) {
+            string = savedInstanceState.getString("string_value", string)
+        }
     }
-}
+    override fun onSaveInstanceState(outState: Bundle) {
+        // put string value
+        super.onSaveInstanceState(outState)
+        outState.putString("string_value", string)
+    }
 
+    /*
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        string = savedInstanceState.getString("string_value", string)
+    }
+     */
+}
 private fun clearButtonVisibility(s: CharSequence?): Int {
     return if (s.isNullOrEmpty()) {
         View.GONE
